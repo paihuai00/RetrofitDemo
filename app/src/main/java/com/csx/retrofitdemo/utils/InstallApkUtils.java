afbcf3d1f917d9edb10ab0f34a2d01a65768a360
cuisx <csx123456>
2019-12-27 17:15:06 +0800

    完善apk下载安装

--- app/src/main/java/com/csx/retrofitdemo/utils/InstallApkUtils.java
+++ app/src/main/java/com/csx/retrofitdemo/utils/InstallApkUtils.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v4.content.FileProvider;
+import com.csx.retrofitdemo.BuildConfig;
 import java.io.File;
 
 /**
@@ -15,19 +16,19 @@ import java.io.File;
 public class InstallApkUtils {
 
     /**
-     * @param file
+     * @param apkFile ：  需要安装的apk
      * @return
      * @Description 安装apk
      */
-    public static void installApk(Context context,File file) {
+    public static void installApk(Context context,File apkFile) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) { // 7.0+以上版本
-            Uri apkUri = FileProvider.getUriForFile(context, "com.retrofitdemo.fileprovider", file);  //包名.fileprovider
+            Uri apkUri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID+ ".fileprovider", apkFile);  //包名.fileprovider
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
         } else {
-            intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+            intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
         }
         context.startActivity(intent);
     }
